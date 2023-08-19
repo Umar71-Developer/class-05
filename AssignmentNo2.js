707ad896bcd6ff476de439a77b6455ac4b9923b2
Umar71-Developer <141718806+Umar71-Developer@users.noreply.github.com>
2023-08-19 05:22:54 -0700

    Add files via upload

---
+++ AssignmentNo2.js
@@ -0,0 +1,9 @@
+"use strict";
+//Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+var tempIncelsius = 37;
+var celToF = (tempIncelsius * 9 / 5) + 32;
+console.log(celToF);
+//From Fahrenheit to Celsius.
+var tempInC = 104;
+var fTocelsius = (tempInC * 5 / 9) - 32;
+console.log(fTocelsius);
---
+++ AssignmentNo2.ts
@@ -0,0 +1,8 @@
+//Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+var tempIncelsius:number= 37;
+var celToF:number= (tempIncelsius*9/5)+32;
+console.log(celToF);
+//From Fahrenheit to Celsius.
+var tempInC:number= 104;
+var fTocelsius:number= (tempInC*5/9)-32;
+console.log(fTocelsius);
